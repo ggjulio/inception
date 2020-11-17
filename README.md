4d672e71808a5dddd1b054279e2908a89a66486a
ggjulio <juligonz@student.42.fr>
2020-11-17 12:19:10 +0100

    mess

--- README.md
+++ README.md
@@ -21,3 +21,22 @@ https://github.com/kelseyhightower/kubernetes-the-hard-way
 
 https://medium.com/faun/metallb-configuration-in-minikube-to-enable-kubernetes-service-of-type-loadbalancer-9559739787df
 https://medium.com/@shoaib_masood/metallb-network-loadbalancer-minikube-335d846dfdbe  
+
+#### dockerfile : Entrypoints + CMDs + Signals and processes
+https://stackoverflow.com/questions/39082768/what-does-set-e-and-exec-do-for-docker-entrypoint-scripts
+https://stackoverflow.com/questions/32255814/what-purpose-does-using-exec-in-docker-entrypoint-scripts-serve/32261019#32261019
+https://stackoverflow.com/questions/50898134/what-does-docker-stopsignal-do
+https://hynek.me/articles/docker-signals/
+https://ubuntu.com/blog/avoiding-dropped-connections-in-nginx-containers-with-stopsignal-sigquit
+https://stackoverflow.com/questions/36781372/docker-using-gosu-vs-user
+https://docs.docker.com/develop/develop-images/dockerfile_best-practices/
+#### MYSQL
+
+https://hub.docker.com/_/mysql
+https://github.com/docker-library/mysql/blob/master/5.6/Dockerfile
+https://github.com/docker-library/mysql/blob/master/5.6/docker-entrypoint.sh
+https://mariadb.com/kb/en/installing-system-tables-mysql_install_db/
+https://dev.mysql.com/doc/refman/5.6/en/mysql-install-db.html
+https://mariadb.com/kb/en/configuring-mariadb-for-remote-client-access/
+https://stackoverflow.com/questions/14779104/mysql-how-to-allow-remote-connection-to-mysql
+https://success.mirantis.com/article/use-a-script-to-initialize-stateful-container-data
--- srcs/images/mysql/docker-entrypoint.sh
+++ srcs/images/mysql/docker-entrypoint.sh
@@ -1,2 +1,57 @@
 #!/bin/sh
 
+set -e
+
+export DATADIR=/var/lib/mysql
+
+logging(){
+	local type=$1; shift
+	printf 'Entrypoint [%s] : %b\n' "$type" "$*"
+}
+
+log_info(){
+	logging 'info' "$@"
+}
+
+log_error(){
+	logging 'error' "$@"
+	exit 1
+}
+
+
+# CREATE USER 'groot'@'172.%' IDENTIFIED BY 'icantstopthisfeelingdeepinsideofme';
+# GRANT ALL ON *.* TO 'groot'@'172.%' WITH GRANT OPTION ;
+# FLUSH PRIVILEGES;
+# DROP DATABASE IF EXISTS test;
+
+# CREATE USER 'root'@'${MYSQL_ROOT_HOST}' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
+# GRANT ALL ON *.* TO 'root'@'${MYSQL_ROOT_HOST}' WITH GRANT OPTION ;
+
+
+# MYSQL_GROOT_HOST		= 172.%
+# MYSQL_GROOT_PASSWORD	= icantstopthisfeelingdeepinsideofme
+# MYSQL_DATABASE		= wordpress
+# MYSQL_USER			= wordpress
+# MYSQL_PASSWORD		= wordpress
+
+# Exit if one variable is missing :
+	# MYSQL_GROOT_HOST		= 172.%
+	# MYSQL_GROOT_PASSWORD	= icantstopthisfeelingdeepinsideofme
+	# MYSQL_DATABASE		= wordpress
+	# MYSQL_USER			= wordpress
+	# MYSQL_PASSWORD		= wordpress
+if [ -z "$MYSQL_GROOT_HOST" -o -z "$MYSQL_GROOT_PASSWORD" -o -z "$MYSQL_DATABASE" -o -z "$MYSQL_USER" -o -z "$MYSQL_PASSWORD" ]; then
+	log_error "Missing one of theses environment variables :\n\tMYSQL_GROOT_HOST\n\tMYSQL_GROOT_PASSWORD\n\tMYSQL_DATABASE\n\tMYSQL_USER\n\tMYSQL_PASSWORD"
+fi
+
+echo "SHHHH" "$BASH_SOURCE"
+if [ ! -d $DATADIR/mysql ]; then
+# if there is no database, initialize database directory ( == if first time container run...)
+	log_info 'Initialize mysql database (at first startup only)'
+	mysql_install_db --user=mysql --datadir=$DATADIR > /dev/null
+else
+	log_info 'Skipping initialization. Mysql database already created.'
+fi
+
+
+exec "$@"
