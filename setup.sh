3730e4d1fea7f3b1ac8e36748220be2beab0bc6e
ggjulio <juligonz@student.42.fr>
2020-12-17 12:26:28 +0100

    .

--- setup.sh
+++ setup.sh
@@ -9,10 +9,16 @@ minikube addons enable dashboard
 IP=$(minikube ip)
 eval $(minikube docker-env)
 
+echo "nginx"
 docker build -t my-nginx srcs/images/nginx/ > /dev/null
+echo "mysql"
 docker build -t my-mysql srcs/images/mysql/ > /dev/null
+echo "fpm"
 docker build -t my-php-fpm srcs/images/php-fpm/ > /dev/null
+echo "done images"
 
+kubectl apply --filename srcs/k8s_objects/configMap-metallb.yaml
+kubectl apply --filename srcs/k8s_objects/nginx.yaml
 #kubectl apply --filename srcs/k8s_objects/
 
 # minikube dashboard
