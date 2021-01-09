79b269afb5258d32d30aa4603f389a54c4e4f546
ggjulio <juligonz@student.42.fr>
2021-01-09 07:54:13 +0100

    ftps done

--- srcs/images/ftps/docker-entrypoint.sh
+++ srcs/images/ftps/docker-entrypoint.sh
@@ -7,7 +7,7 @@ if [ -z "$USER" -o -z "$PASSWORD"  ]; then
 	exit 1
 fi
 if [ -z $(id -u "$USER" 2> /dev/null) ]; then
-	adduser -D --shell /bin/sh "$USER" "$USER"
+	adduser -D --shell /bin/ash "$USER" "$USER"
 	echo "user $USER created"
 	echo "$USER:$PASSWORD" | chpasswd
 	mkdir -p /ftps_data/
@@ -15,13 +15,14 @@ if [ -z $(id -u "$USER" 2> /dev/null) ]; then
 	chown -R $USER /ftps_data
 	chgrp -R $USER /ftps_data
 
-	# Log issue with redirect to stdout : https://github.com/million12/docker-vsftpd/pull/8
 	mkdir -p /var/log/vsftpd
 	touch /var/log/vsftpd/vsftpd.log
-	# tail -f /var/log/vsftpd/vsftpd.log | tee /dev/stdout &
-	# ln -sf /dev/stdout /var/log/vsftpd/vsftpd.log 
 else
 	echo 'user already exist, skip user creation'
 fi
 
-exec "$@"
+# Log issue, redirection to stdout : https://github.com/million12/docker-vsftpd/pull/8
+# tail -f /var/log/vsftpd/vsftpd.log | tee /dev/stdout &
+# ln -sf /dev/stdout /var/log/vsftpd/vsftpd.log 
+# exec "$@" running vsftpd as PID 1 provoke crash of vsftpd process while disconneted.
+"$@"
