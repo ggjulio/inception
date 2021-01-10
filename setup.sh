2e3aebae2e93e9f0071d857f1e22501c7e899c20
ggjulio <juligonz@student.42.fr>
2021-01-10 05:22:49 +0100

    now telegraf kubernetes input work

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ if [ "${1-0}" = "delete" -o "${1-0}" = "clean" ]; then		#
 fi															#
 #############################################################
 
-minikube start --vm-driver=docker
+minikube start --vm-driver=docker --memory 3100   --bootstrapper=kubeadm
 minikube addons enable metrics-server
 minikube addons enable dashboard
 # Allow connection to k8s api... for the telegraf inputs.kubernetes plugin
@@ -21,12 +21,12 @@ kubectl create clusterrolebinding node-admin-default-svc --clusterrole=cluster-a
 #############################################################
 ######## build images inside minikube  container ############
 eval $(minikube docker-env)
+docker build -t my-telegraf srcs/images/telegraf > /dev/null
 docker build -t my-nginx srcs/images/nginx/ > /dev/null
 docker build -t my-mysql srcs/images/mysql/ > /dev/null
 docker build -t my-php-fpm srcs/images/php-fpm/ > /dev/null
 docker build -t my-influxdb srcs/images/influxdb > /dev/null
 docker build -t my-grafana srcs/images/grafana > /dev/null
-docker build -t my-telegraf srcs/images/telegraf > /dev/null
 docker build -t my-ftps srcs/images/ftps > /dev/null
 eval $(minikube docker-env --unset)
 
@@ -41,15 +41,15 @@ docker network create --subnet=172.20.0.0/16 my-net
 docker network connect my-net minikube
 
 
-kubectl apply --filename srcs/k8s_objects/secrets.yaml
 kubectl apply --filename srcs/k8s_objects/nginx.yaml
-kubectl apply --filename srcs/k8s_objects/mysql.yaml
 kubectl apply --filename srcs/k8s_objects/wordpress.yaml
-kubectl apply --filename srcs/k8s_objects/phpmyadmin.yaml
-kubectl apply --filename srcs/k8s_objects/influxdb.yaml
 kubectl apply --filename srcs/k8s_objects/grafana.yaml
-kubectl apply --filename srcs/k8s_objects/telegrafDaemonSet.yaml
+kubectl apply --filename srcs/k8s_objects/phpmyadmin.yaml
 kubectl apply --filename srcs/k8s_objects/ftps.yaml
+kubectl apply --filename srcs/k8s_objects/secrets.yaml
+kubectl apply --filename srcs/k8s_objects/influxdb.yaml
+kubectl apply --filename srcs/k8s_objects/mysql.yaml
+kubectl apply --filename srcs/k8s_objects/telegrafDaemonSet.yaml
 #kubectl apply --filename srcs/k8s_objects/
 
 minikube dashboard
