6809d8758cb1dd94096adabb2e62ac892123e6a9
ggjulio <juligonz@student.42.fr>
2021-06-18 17:41:20 +0200

    .

--- srcs/requirements/wordpress/docker-entrypoint.sh
+++ srcs/requirements/wordpress/docker-entrypoint.sh
@@ -9,6 +9,8 @@ else
 	wget -q -O - https://github.com/WordPress/WordPress/archive/5.6.tar.gz | tar -xz -C /var/www/app --strip-components=1
 	echo "Done."
 	mv /requirements/wp-config.php /var/www/app;
+	wp --path=/var/www/app/  plugin install redis-cache
+	wp --path=/var/www/app/  plugin activate redis-cache
 fi
 
-exec php-fpm7
+exec "$@"
